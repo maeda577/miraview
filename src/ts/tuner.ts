33776e88f454d63e18360871c586076e492ae904
maeda577 <kazu55632000@gmail.com>
2025-01-26 22:29:18 +0900

    tuner整理

--- src/ts/tuner.ts
+++ src/ts/tuner.ts
@@ -7,59 +7,60 @@
  * LICENSE file in the root directory of this source tree.
  */
 import createClient from 'openapi-fetch/dist/index.js';
-import type { paths } from './types/mirakc.d.ts';
+import type { paths, components } from './types/mirakc.d.ts';
 import { loadConfigFromStorage } from './utils/common.js';
 import './utils/ix.js';
 
-// APIアクセス用のクライアント
-const config = loadConfigFromStorage();
-const client = createClient<paths>({ baseUrl: config.getApiEndpoint() });
+// チューナー一覧を取得
+async function getTuners() {
+  const client = createClient<paths>({ baseUrl: loadConfigFromStorage().getApiEndpoint() });
+  try {
+    return (await client.GET("/tuners")).data;
+  } catch (error) {
+    return undefined;
+  }
+}
 
-const cardList = document.getElementById('card-list')!;
+// チューナーの利用状況に応じて色を変える
+function getVariant(tuner: components['schemas']['MirakurunTuner']) {
+  if (tuner.isFree) {
+    return 'success';
+  } else if (Math.max(...tuner.users.map(u => u.priority)) <= 0) {
+    return 'warning';
+  } else {
+    return 'alarm';
+  }
+}
 
 // 画面更新
 async function refreshTuners() {
-  // APIを叩く
-  const response = await client.GET("/tuners");
-  if (response.error !== undefined) {
-    console.error(response.error);
-    return;
-  }
-
+  const tuners = await getTuners();
   // 更新ボタンを残してチューナーの要素を全部消す
+  const cardList = document.getElementById('card-list')!;
   cardList.replaceChildren(cardList.children[0]);
 
-  response.data?.forEach(tuner => {
-    // チューナーの利用状況に応じて色を変える
-    let variant: string;
-    if (tuner.isFree) {
-      variant = 'success';
-    } else if (Math.max(...tuner.users.map(u => u.priority)) <= 0) {
-      variant = 'warning';
-    } else {
-      variant = 'alarm';
-    }
-
+  tuners?.forEach(tuner => {
     cardList.insertAdjacentHTML('beforeend', `
       <ix-card variant="filled">
         <ix-card-content>
           <span class="typography-display">${tuner.name}</span>
           <span class="typography-label">${tuner.types.join(' : ')}</span>
-          <ix-pill variant="${variant}">${tuner.isFree ? 'Free' : 'Using'}</ix-pill>
+          <ix-pill variant="${getVariant(tuner)}">${tuner.isFree ? 'Free' : 'Using'}</ix-pill>
           <ix-key-value-list>
             <ix-key-value label="Command" value="${tuner.command}" ${tuner.command ? '' : 'hidden'}></ix-key-value>
-              ${tuner.users.map(user => `
-                <ix-key-value label="User ID" value="${user.id}"></ix-key-value>
-                <ix-key-value label="Priority" value="${user.priority}"></ix-key-value>
-                <ix-key-value label="User Agent" value="${user.agent}" ${user.agent ? '' : 'hidden'}></ix-key-value>
-              `).join('')}
+            ${tuner.users.map(user => `
+              <ix-key-value label="User ID" value="${user.id}"></ix-key-value>
+              <ix-key-value label="Priority" value="${user.priority}"></ix-key-value>
+              <ix-key-value label="User Agent" value="${user.agent}" ${user.agent ? '' : 'hidden'}></ix-key-value>
+            `).join('')}
           </ix-key-value-list>
         </ix-card-content>
       </ix-card>`);
   });
 }
 
-// 画面更新
+// 即時に画面更新
 refreshTuners();
 
-cardList.querySelector('ix-action-card')?.addEventListener('click', refreshTuners);
+// 更新カードにイベントを割り当てる
+document.getElementById('card-refresh')?.addEventListener('click', refreshTuners);
