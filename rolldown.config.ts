a725e58f08818fa4703a8adf74e79b8dc4f83e73
maeda577 <kazu55632000@gmail.com>
2026-05-10 14:04:39 +0900

    npm+rolldownに変更

---
+++ rolldown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'rolldown';
+
+export default defineConfig({
+  input: [
+    'src/timetable.ts',
+  ],
+  output: {
+    dir: 'dist/js',
+    cleanDir: true,
+    preserveModules: true,
+    preserveModulesRoot: 'src/'
+  },
+});
